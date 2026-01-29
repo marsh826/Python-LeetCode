f5cad3b3a35a4fd421f2b328cd146a6f4a7f5b9b
marsh826 <lean7054@gmail.com>
2026-01-30 00:27:26 +1000

    leave this as it is for now

--- TwoSums.py
+++ TwoSums.py
@@ -16,22 +16,24 @@
 
 # Extended challenge: Optimise the above answer to below O(n^2)
 # Target time complexity: O(n)
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            ans = target - nums[i]
-            if ans in nums and nums.index(ans) != i:
-                return [i, nums.index(ans)]
+
+#Attempt 1:
+# class Solution:
+#     def twoSum(self, nums: list[int], target: int) -> list[int]:
+#         for i in range(len(nums)):
+#             ans = target - nums[i]
+#             if ans in nums and nums.index(ans) != i:
+#                 return [i, nums.index(ans)]
             
 
 # nums = [3, 2, 3]
 # target = 6
-nums = [2,7,11,15]
-target = 9
+# nums = [2,7,11,15]
+# target = 9
 
-test = Solution()
-print(test.twoSum(nums, target))
-# LeetCode Feedback to the Result of Extended Challenge:
+# test = Solution()
+# print(test.twoSum(nums, target))
+# LeetCode Feedback:
 # Execution Speed: 535 ms
 # Time Complexity: O(n^2)
 # Beats (Speed): 31.55%
